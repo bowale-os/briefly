40dbe2b3b811f0f7f90b547a02838440f1d25b2d
Daniel Sobowale <danbowale@gmail.com>
2026-02-16 16:42:30 -0600

    changed env to have prefix: next_public

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
-const API_BASE_URL = process.env.NEXT_API_URL || 'http://127.0.0.1:8000'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000'
 
 export const api: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
