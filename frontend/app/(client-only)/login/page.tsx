3c2b7f5183d7319bbc38be3047af0b143e1189e0
Daniel Sobowale <danbowale@gmail.com>
2026-03-03 02:17:41 -0600

    login.tsx

--- frontend/app/(client-only)/login/page.tsx
+++ frontend/app/(client-only)/login/page.tsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic' as const
 'use client'
+
+export const dynamic = 'force-dynamic' as const
 import { useState, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { motion } from 'framer-motion'
